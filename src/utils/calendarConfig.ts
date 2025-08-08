23723389e2346bea83e0b770d39914182ad4e858
pearl <pearlodi7@gmail.com>
2025-08-07 17:26:34 -0700

    updates

--- src/utils/calendarConfig.ts
+++ src/utils/calendarConfig.ts
@@ -1,6 +1,6 @@
 import { format, parse, startOfWeek, getDay } from "date-fns";
 import { dateFnsLocalizer } from "react-big-calendar";
-import { enUS } from "/Users/mac/Documents/webapps/bible-app/node_modules/date-fns/locale/en-US"
+import {enUS} from "date-fns/locale/en-US";
 const locales = {
     "en-US": enUS,
   };
